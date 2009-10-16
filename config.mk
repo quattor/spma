498c8114bcf2fac697668f1fafaf22ddf58b4107
iven <iven@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2009-10-16 13:17:44 +0000

    2009-10-16  Jan IVEN
    	* Release: 1.10.34
    	- implement "protectkernel" option (#49733), some more debug output
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/spma@1318 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 COMP=spma
 NAME=$(COMP)
 DESCR=SPM Agent, quattor toolsuite
-VERSION=1.10.33
+VERSION=1.10.34
 RELEASE=1
 AUTHOR=German Cancio <German.Cancio@cern.ch>
 MAINTAINER=German Cancio <German.Cancio@cern.ch
@@ -12,4 +12,4 @@ MAN3DIR=$(QTTR_MAN)/man$(MANSECT)
 MAN1DIR=$(QTTR_MAN)/man1
 
 
-DATE=17/07/08 15:51
+DATE=16/10/09 15:15
