f95331c1c6f6bd8f83e9a90e78cdb88e7e5ab537
verosvn <verosvn@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-08-08 14:49:10 +0000

    2011-08-08  Veronique Lefebure,31 1-022,,
    	* Release: 1.11.8
    	- pass noaction option to ncm-grub in spma_wrapper.sh
    
    
    
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/spma@2921 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 COMP=spma
 NAME=$(COMP)
 DESCR=SPM Agent, quattor toolsuite
-VERSION=1.11.7
+VERSION=1.11.8
 RELEASE=1
 AUTHOR=German Cancio <German.Cancio@cern.ch>
 MAINTAINER=German Cancio <German.Cancio@cern.ch
@@ -12,4 +12,4 @@ MAN3DIR=$(QTTR_MAN)/man$(MANSECT)
 MAN1DIR=$(QTTR_MAN)/man1
 
 
-DATE=14/07/11 10:48
+DATE=08/08/11 16:48
